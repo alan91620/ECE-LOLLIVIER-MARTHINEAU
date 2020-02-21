299cccb968d20b36149834e721a2f1561bbfb690
alanm <alan.marthineau@laposte.net>
2020-02-21 17:55:54 +0100

    Adding CRUD

--- ECE-LOLLIVIER-MARTHINEAU/Program.cs
+++ ECE-LOLLIVIER-MARTHINEAU/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECE.AA.MyAirport.EF;
+using System;
+using System.Linq;
 
 namespace ECE.AA.MyAirport.App
 {
@@ -6,6 +8,42 @@ namespace ECE.AA.MyAirport.App
     {
         static void Main(string[] args)
         {
+            using (var db = new AirportContext())
+            {
+
+                //CREATE
+                var vol = new Vol { 
+                    CIE = 1,
+                    LIG = "AZERTY",
+                    JEX = 1
+                };
+                db.Vols.Add(vol);
+                db.SaveChanges();
+
+                var baggage = new Bagage
+                {
+                    ID_VOL = 1,
+                    CODE_IATA = "AZERTY"
+                };
+                db.Bagages.Add(baggage);
+                db.SaveChanges();
+
+                // Read
+                var bag = db.Bagages
+                    .First();
+
+                // Update
+                bag.CODE_IATA = "TREZA";
+                bag.SSUR = "AZE";
+                db.SaveChanges();
+
+                // Delete
+                db.Remove(bag);
+                db.SaveChanges();
+
+
+
+            }
             Console.WriteLine("Hello World!");
         }
     }
