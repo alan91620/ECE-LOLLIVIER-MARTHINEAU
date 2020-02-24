317b2dcc27ff1c6f912eb43d5d9006b25e5d2568
alanm <alan.marthineau@laposte.net>
2020-02-24 17:40:58 +0100

    logger added

--- MyAirport.EF/AirportContext.cs
+++ MyAirport.EF/AirportContext.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ECE.AA.MyAirport.EF
+
+
 {
     public class AirportContext : DbContext
     {
+
+        public static readonly ILoggerFactory MyLoggerFactory
+    = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
         public DbSet<Vol> Vols { get; set; }
         public DbSet<Bagage> Bagages { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            /*optionsBuilder.UseSqlServer(
                 @"Server=(localdb)\mssqllocaldb;Database=Airport;Integrated Security=True");
+                */
+
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["AirportDB"].ConnectionString);
         }
     }
 }
